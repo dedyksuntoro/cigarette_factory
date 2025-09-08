7f94e02bdb54fe4805596fbde4ff9331e4fab871
Dedyk Suntoro Atmojo <dedyk.ds@gmail.com>
2025-09-09 00:44:26 +0700

    Update header.php

--- page/templates/header.php
+++ page/templates/header.php
@@ -61,57 +61,7 @@ $role = $_SESSION['role'];
         <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas"></button>
     </div>
     <div class="offcanvas-body">
-        <h4 class="text-center">Cigarette Factory</h4>
-<hr class="bg-light">
-<ul class="nav flex-column">
-    <li class="nav-item">
-        <a class="nav-link" href="<?php echo $_ENV['BASE_URL']; ?>/index.php">Dashboard</a>
-    </li>
-    <?php if ($role == 'admin') { ?>
-        <li class="nav-item">
-            <a class="nav-link" href="<?php echo $_ENV['BASE_URL']; ?>/page/users/list.php">Manajemen Pengguna</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="<?php echo $_ENV['BASE_URL']; ?>/page/logs/list.php">Log Aktivitas</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="<?php echo $_ENV['BASE_URL']; ?>/page/employees/list.php">Manajemen Karyawan</a>
-        </li>
-    <?php } ?>
-    <?php if (in_array($role, ['admin', 'supervisor'])) { ?>
-        <li class="nav-item">
-            <a class="nav-link" href="<?php echo $_ENV['BASE_URL']; ?>/page/production_plans/list.php">Perencanaan Produksi</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="<?php echo $_ENV['BASE_URL']; ?>/page/production_results/list.php">Hasil Produksi</a>
-        </li>
-    <?php } ?>
-    <?php if (in_array($role, ['admin', 'gudang'])) { ?>
-        <li class="nav-item">
-            <a class="nav-link" href="<?php echo $_ENV['BASE_URL']; ?>/page/materials/list.php">Manajemen Bahan Baku</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="<?php echo $_ENV['BASE_URL']; ?>/page/finished_goods/list.php">Manajemen Barang Jadi</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Pergerakan Stok</a>
-        </li>
-    <?php } ?>
-    <?php if (in_array($role, ['admin', 'keuangan'])) { ?>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Faktur</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Pengeluaran</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Pendapatan</a>
-        </li>
-    <?php } ?>
-    <li class="nav-item">
-        <a class="nav-link" href="<?php echo $_ENV['BASE_URL']; ?>/page/auth/logout.php">Logout</a>
-    </li>
-</ul>
+        <?php include 'sidebar.php'; ?>
     </div>
 </div>
 
