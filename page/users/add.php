9409cab21e9a5b403808b4a80b049ecb47a1523c
Dedyk Suntoro Atmojo <dedyk.ds@gmail.com>
2025-09-08 23:16:04 +0700

    Update add.php

--- page/users/add.php
+++ page/users/add.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-require_once __DIR__.'../../config/db.php';
-require_once __DIR__.'../templates/header.php';
+require_once __DIR__.'/../../config/db.php';
+require_once __DIR__.'/../templates/header.php';
 
 // Cek apakah pengguna sudah login dan memiliki peran admin
 if (!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'admin') {
