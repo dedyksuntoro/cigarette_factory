02a1cc5db05b9a4ec3ca2f45f7c91405b861962c
Dedyk Suntoro Atmojo <dedyk.ds@gmail.com>
2025-09-21 10:59:25 +0700

    Update edit permissions

--- page/permissions/edit.php
+++ page/permissions/edit.php
@@ -47,7 +47,7 @@ require_once __DIR__.'/../templates/header.php';
 ?>
 
 <div class="container mt-4">
-    <h1>Edit izin Pengguna</h1>
+    <h1>Edit Permission</h1>
     <?php if (isset($error)) { ?>
         <div class="alert alert-danger"><?php echo $error; ?></div>
     <?php } ?>
